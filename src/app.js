11199c59b0331dd875994980209d650171610766
itsaiub <itsaiub@gmail.com>
2019-10-04 14:36:27 +0600

    babel-webpack configuration

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { person } from './lib';
+
+const testFunc = () => {
+	console.log('Hello World!');
+};
+testFunc();
+
+console.log(person.name);
+
+async function getPosts() {
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const data = await response.json();
+
+	return data;
+}
+
+getPosts()
+	.then((posts) => {
+		console.log(posts);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
---
+++ src/lib.js
@@ -0,0 +1,5 @@
+export const person = {
+	name: 'Ratul',
+	location: 'Dhaka',
+	age: 23
+};
